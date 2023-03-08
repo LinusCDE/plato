b97f04a1635a24e3a8510a07cbaed0ebe8474a79
Linus <linus@cosmos-ink.net>
2023-03-08 18:37:44 +0100

    Prevent downloading mupdf headers multiple times
    It would default in multiple numbered files which aren't used anyways.

--- download_headers.sh
+++ download_headers.sh
@@ -3,9 +3,13 @@
 # Get mupdf include/header files
 MUPDF_VERSION="1.17.0"
 MUPDF_ARCHIVE="mupdf-$MUPDF_VERSION-source.tar.xz"
-echo "Downloading $MUPDF_ARCHIVE"
 
-wget -q --show-progress "https://mupdf.com/downloads/archive/$MUPDF_ARCHIVE"
+if [ ! -f "$MUPDF_ARCHIVE" ]; then
+    echo "Downloading $MUPDF_ARCHIVE"
+    wget -q --show-progress "https://mupdf.com/downloads/archive/$MUPDF_ARCHIVE"
+else
+    echo "$MUPDF_ARCHIVE was already downloaded"
+fi
 
 mkdir -p thirdparty/mupdf
 tar -C thirdparty/mupdf --wildcards --strip-components=1 -xvJf "$MUPDF_ARCHIVE" 'mupdf-*-source/include'
